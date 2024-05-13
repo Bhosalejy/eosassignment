c9c6d51a44886ed51927dd6e46131d9113d5d5f6
E1_Jyoti_83865 <jsbhosale.com>
2024-05-13 18:21:30 +0530

    eosassignment

--- day_8/Q2.c
+++ day_8/Q2.c
@@ -1,27 +1,27 @@
 
-
-#include<unistd.h>
 #include<stdio.h>
+#include<unistd.h>
+#include<sys/wait.h>
 
-int main(){
-    int ret;
-	int s, i, j;
-	while(1){
-	ret = fork();
-	if(ret==0){
-	for(i=1; i<=5; i++) {
-			printf("child: %d\n", i);
-			sleep(1);
-		}
-		_exit(0);
+int main (){
+int ret,s,i,j;
+ i=1;
+while (1) {
+     ret=fork();
+	 if(ret ==0){
+	 printf("Jyoti");
+	 _exit(0);
 	}
-	else if(ret== -1)
+	else if( ret== -1)
 	   break;
-	else{
-       i++;
-	   printf("child count: %d\n",i);
-	}   
-	}
-	for(j=1;j<=i;i++)
-	    wait(&s);
+	else {
+	  i++;
+	  printf("child count : %d\n",i);
+	  }
 }
+
+for (i=1;j<=i;j++)
+     wait(&s);
+
+}
+
