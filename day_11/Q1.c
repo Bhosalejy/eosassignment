094798fc24c967d666dec259ba0638bd1f48c2d7
E1_Jyoti_83865 <jsbhosale.com>
2024-05-16 21:41:05 +0530

    eosassignment

--- day_11/Q1.c
+++ day_11/Q1.c
@@ -1,29 +1,31 @@
+/*
+Execute "wc" command from your program (using fork() + exec()).
+The command should read data from the ﬁle "in.txt" (instead of terminal)
+and output should be displayed on terminal.
+*/
+
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/wait.h>
 #include <fcntl.h>
-#include <string.h>
+
 int main() {
 	int ret2, err2, s2,fd;
-	int arr[2], ret;
 	printf("parent started!\n");
-
-	// create pipe
-	ret = pipe(arr);
-	if(ret < 0) {
-		perror("pipe() failed");
-		_exit(3);
-	}
-     
 
 	// run "wc" command
 	ret2 = fork();
 	if(ret2 == 0) {
 		// dup pipe read fd to stdin
 		fd = open("in.txt",O_RDONLY);
-		close(arr[1]);
-		dup2(arr[0], 0);
-		close(arr[0]);
+		close(0);
+		dup(fd);
+		close(fd);
+        
+		// in  redirection
+		fd = read("in.txt", O_CREAT | O_TRUNC | O_WRONLY, 0644);
+		dup2(fd, 0); // close stderr and copy fd on stderr
+		close(fd);
 
 		err2 = execlp("wc", "wc",NULL);
 		if(err2 < 0) {
@@ -31,11 +33,9 @@ int main() {
 			_exit(2);
 		}
 	}
-
+     else
 	// close pipe both ends in parent (no write no read)
-	close(arr[1]);
-	close(arr[0]);
-	waitpid(ret2, &s2, 0);
+	waitpid(-1, &s2, 0);
 	printf("parent completed!\n");
 	return 0;
 }
